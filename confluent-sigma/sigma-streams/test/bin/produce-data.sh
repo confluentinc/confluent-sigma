ec21a7acb052146da8393c3905ef26aca2c2c7c4
William LaForest <lacapaneus@gmail.com>
2022-03-12 09:48:41 -0500

    Added comments and made an infinite loop

--- confluent-sigma/sigma-streams/test/bin/produce-data.sh
+++ confluent-sigma/sigma-streams/test/bin/produce-data.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script creates a topic called test and then loops infinitely sending data from a specified file
+#
+# Parameter 1 the data file to send
+# Parameter 2 the number of paritions for the test topic
+
 source test/bin/ccloud-env.sh
 
 docker run -v $(pwd)/test/config:/mnt/config --rm --network=host confluentinc/cp-server:latest \
@@ -12,9 +17,12 @@ docker run -v $(pwd)/test/config:/mnt/config --rm --network=host confluentinc/cp
   --partitions $2
 
 
-docker run -v $(pwd)/test/data:/mnt/data --rm --network=host edenhill/kafkacat:1.5.0  \
-  kafkacat -b ${CCLOUD_BOOTSTRAP_SERVER} -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN \
-  -X sasl.username=${CCLOUD_API_KEY} -X sasl.password=${CCLOUD_API_SECRET} -t test -l /mnt/data/test-zeek-dns.txt
+while true
+do
+  docker run -v $(pwd)/test/data:/mnt/data --rm --network=host edenhill/kafkacat:1.5.0  \
+    kafkacat -b ${CCLOUD_BOOTSTRAP_SERVER} -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN \
+    -X sasl.username=${CCLOUD_API_KEY} -X sasl.password=${CCLOUD_API_SECRET} -t test -l /mnt/data/$1
+done
 
 
 
