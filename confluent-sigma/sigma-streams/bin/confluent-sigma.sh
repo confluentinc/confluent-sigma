0a4eb1ef4bea2b0f1a9dc403e0398e4ca646a86e
Will LaForest <wlaforest@confluent.io>
2022-03-22 19:40:00 +0000

    enhanced confluent-sigma.sh to take optionally take in a specific config file

--- confluent-sigma/sigma-streams/bin/confluent-sigma.sh
+++ confluent-sigma/sigma-streams/bin/confluent-sigma.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -14,16 +15,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Script for launching the sigma streams app.
+# Script for launching the sigma streams app.  First parameter should be a config
+# properties file.  if its not then we will search the path for a default
 
 PROPS=
 
-if [ -f ~/.config/sigma.properties ] ; then
+if [ -f "$1" ] ; then
+  PROPS=$1
+elif [ -f ~/.config/sigma.properties ] ; then
   PROPS=~/.config/sigma.properties
 elif [ -f ~/.confluent/sigma.properties ] ; then
   PROPS=~/.confluent/sigma.properties
 elif [ -f ~/tmp/sigma.properties ] ; then
   PROPS=~/tmp/sigma.properties
 fi
+
+echo "using $PROPS config"
 
 java -cp target/sigma-streams-1.0-fat.jar io.confluent.sigmarules.SigmaStreamsApp $* -c $PROPS
