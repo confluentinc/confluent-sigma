b162f3db69ec82d4ba03b0b3956a50ed9df86d0d
Will LaForest <wlaforest@confluent.io>
2022-03-22 15:38:06 +0000

    Updated record count script

--- confluent-sigma/sigma-streams/test/bin/record-count.sh
+++ confluent-sigma/sigma-streams/test/bin/record-count.sh
@@ -1,12 +1,34 @@
- if [ -f /tmp/ccloud-env.sh ] ; then
-   source /tmp/ccloud-env.sh
- elif [ -f ~/tmp/ccloud-env.sh ] ; then
-     source  ~/tmp/ccloud-env.sh
- else
-   echo "ccloud-env not found"
-   exit
- fi
+if [ -f /tmp/ccloud-env.sh ] ; then
+  source /tmp/ccloud-env.sh
+elif [ -f ~/tmp/ccloud-env.sh ] ; then
+    source  ~/tmp/ccloud-env.sh
+else
+  echo "ccloud-env not found"
+  exit
+fi
 
- docker run --rm --network=host edenhill/kafkacat:1.5.0  \
-    kafkacat -b ${BOOTSTRAP_SERVER} -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN \
-    -X sasl.username=${KAFKA_SASL_USERNAME} -X sasl.password=${KAFKA_SASL_PASSWORD} -t $1 -L
+PROPS=
+
+if [ -f ~/.config/sigma.properties ] ; then
+  export PROPS=~/.config/
+elif [ -f ~/.confluent/sigma.properties ] ; then
+  export PROPS=~/.confluent/
+elif [ -f ~/tmp/sigma.properties ] ; then
+  export PROPS=~/tmp/
+else
+  echo "sigma properties not found"
+  exit
+fi
+
+CGROUP="COUNT-$1-$RANDOM"
+
+docker run --rm --network=host edenhill/kcat:1.7.1  \
+  kafkacat -b ${BOOTSTRAP_SERVER} -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN \
+  -X sasl.username=${KAFKA_SASL_USERNAME} -X sasl.password=${KAFKA_SASL_PASSWORD} -o beginning -G $CGROUP  -c 50000 $1 > /dev/null
+
+docker run -v ${PROPS}:/mnt/config --rm --network=host confluentinc/cp-server:latest \
+  kafka-consumer-groups --describe --group $CGROUP --bootstrap-server ${BOOTSTRAP_SERVER} \
+  --command-config /mnt/config/sigma.properties     
+
+
+    
