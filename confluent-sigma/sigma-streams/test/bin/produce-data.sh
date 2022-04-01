2e03c6695e08eecf5bbf29e83d10bf1f9adb24e6
William LaForest <lacapaneus@gmail.com>
2022-04-01 11:26:57 -0400

    Parameterized topic name for data production script

--- confluent-sigma/sigma-streams/test/bin/produce-data.sh
+++ confluent-sigma/sigma-streams/test/bin/produce-data.sh
@@ -3,7 +3,8 @@
 # This script creates a topic called test and then loops infinitely sending data from a specified file
 #
 # Parameter 1 the data file to send
-# Parameter 2 the number of paritions for the test topic
+# Parameter 2 the topic name
+# Parameter 3 the number of partitions to for the topic
 
 source bin/auto-configure.sh
 
@@ -14,14 +15,14 @@ docker run -v $(pwd)/test/config:/mnt/config --rm --network=host confluentinc/cp
   --topic test \
   --create \
   --replication-factor 3 \
-  --partitions $2
+  --partitions $3
 
 
 while true
 do
   docker run -v $(pwd)/test/data:/mnt/data --rm --network=host edenhill/kafkacat:1.5.0  \
     kafkacat -b ${BOOTSTRAP_SERVER} -X security.protocol=SASL_SSL -X sasl.mechanisms=PLAIN \
-    -X sasl.username=${KAFKA_SASL_USERNAME} -X sasl.password=${KAFKA_SASL_PASSWORD} -z snappy -t test -l /mnt/data/$1
+    -X sasl.username=${KAFKA_SASL_USERNAME} -X sasl.password=${KAFKA_SASL_PASSWORD} -z snappy -t $2 -l /mnt/data/$1
 done
 
 
