3670fc930fbc704037f52928cc1ff5cace8500a3
Will LaForest <lacapaneus@gmail.com>
2023-06-12 12:40:12 -0400

    Moving testing data collection logic into python from bash

--- confluent-sigma/sigma-streams/test/bin/check-process-rate.sh
+++ confluent-sigma/sigma-streams/test/bin/check-process-rate.sh
@@ -11,18 +11,9 @@ if [ -f "$SCRIPT_DIR/../../bin/auto-configure.sh" ] ; then
   source $SCRIPT_DIR/../../bin/auto-configure.sh
 fi
 
-
-if [ ! -f $SIGMA_PROPS ] ; then
-  echo "sigma properties not found.  Suggested path from auto-configure is $SIGMA_PROPS"
+if [ ! -f "$SIGMA_CC_ADMIN" ] ; then
+  echo "sigma cc admin properties not found."
   exit -1
 fi
 
-saslUsername=$(grep "sasl.jaas.config" $SIGMA_PROPS | sed "s/.*username='\([^']*\)'.*/\1/")
-saslPassword=$(grep "sasl.jaas.config" $SIGMA_PROPS | sed "s/.*password='\([^']*\)'.*/\1/")
-combined="${saslUsername}:${saslPassword}"
-creds=$(echo "$combined" | base64)
-
-curl 'https://api.telemetry.confluent.cloud/v2/metrics/cloud/query' \
--H "Content-Type: application/json" \
--d '{"aggregations":[{"metric":"io.confluent.kafka.server/sent_bytes"}],"filter":{"filters":[{"field":"resource.kafka.id","op":"EQ","value":"lkc-7yyp22"},{"field":"metric.topic","op":"EQ","value":"test2"}],"op":"AND"},"granularity":"PT1M","intervals":["2023-06-07T15:33:00-04:00/2023-06-07T16:33:00-04:00"],"limit":1000}' \
--H "Authorization: Basic $creds"
+python3 "$SCRIPT_DIR"/check-process-rate.py
